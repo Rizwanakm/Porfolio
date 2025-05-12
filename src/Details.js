4c7848a22ac0482e0f92c2b47d82b5e7b69a1b73
Rizwanakm <you@example.com>
2025-05-12 23:57:22 +0530

    first commit

---
+++ src/Details.js
@@ -0,0 +1,135 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.png";
+import logo from "./assets/logo2.png";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Rizwana",
+  tagline: "I build things for web",
+  img: profile,
+ about: `I am a passionate full stack web developer with a strong interest in creating dynamic, user-friendly web applications. Having recently completed my B.Tech in Computer Science, I am currently focused on honing my front-end skills using technologies like React, HTML, CSS, and JavaScript. I enjoy building responsive, high-performance websites and am always eager to expand my technical toolkit. Feel free to connect with me if you'd like to collaborate or learn more!`,
+}
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/rizwanamolkm/",
+  github: "https://github.com/Rizwanakm/",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Full stack developer intern",
+    Company: `Edurevo`,
+    Location: "Kozhikode",
+    Type: "Full Time",
+    Duration: "March-2025,Present",
+  },
+ 
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Bachelor of Technology in Computer Science & Engineering",
+    Company: "Government Model Engineering College",
+    Location: "Kochi",
+    Type: "Full Time",
+    Duration: "Sept 2022 - April 2025",
+  },
+  {
+    Position: "Diploma in computer engineering",
+    Company: `Government Womens Polytechnic College`,
+    Location: "Ernakulam",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Mar 2022",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Architectural Plan Generation using Stable Diffusion Model",
+    image: projectImage1,
+    description: `Developed Architectural Plan Generation using Stable Diffusion Model , a website that generate architectural plan by integrating user
+preferences using HTML, CSS, JavaScript, Python Flask`,
+    techstack: "HTML/CSS, JavaScript ,Python Flask",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Rizwanakm/Arch-AI.git",
+  },
+  {
+    title: " Kiddo Shield",
+    image: projectImage2,
+    description: `Developed Kiddo Shield, a website dedicated to managing vaccination records for children aged 0-15 years old using HTML, CSS, JavaScript,
+Django`,
+    techstack: "HTML/CSS, JavaScript ,Django",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Rizwanakm/kiddo-shield.git",
+  },
+  {
+    title: " Labour Management System",
+    image: projectImage3,
+    description: `Developed Labour Management System, a website which manages Migrated Labours in a country, using React and Node.js.`,
+    techstack: "HTML/CSS, JavaScript, React and Node.js",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/Rizwanakm",
+  },
+ 
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "rizwanamolkm2000@gmail.com",
+  phone: "+91 7736583436",
+};
